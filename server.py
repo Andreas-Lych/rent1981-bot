2621a649fcafc403aa68f60a63e4d09e14b30db5
Andreas-Lych <caesar.a.1981@gmail.com>
2023-05-23 23:54:04 +0300

    swagger, marschmallow

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from json import JSONDecodeError
+import logging
+from json import JSONDecodeError, loads
 
-from aiohttp import web
+from aiohttp import WSMessage, WSMsgType, web
 from aiohttp_apispec import docs, request_schema, setup_aiohttp_apispec
 from marshmallow import ValidationError
 
@@ -9,15 +10,8 @@ from schemas import MessageSchema
 
 routes = web.RouteTableDef()
 
-@docs(
-   tags=["telegram"],
-   summary="Send message API",
-   description="This end-point sends message to telegram bot user/users",
-)
-@routes.get("/")
-async def index_get(request: web.Request) -> web.Response:
-    await send_message_to_all("Hello")
-    return web.json_response({"result": "OK"})
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @docs(
@@ -45,10 +39,43 @@ async def index_post(request: web.Request) -> web.Response:
     return web.json_response({"result": "OK"})
 
 
+@docs(
+   tags=["telegram"],
+   summary="Get status",
+   description="Return status of the server",
+)
+@routes.get("/")
+async def index_get(request: web.Request) -> web.Response:
+    return web.json_response({"result": "OK"})
+
+
+@docs(
+   tags=["websocket"],
+   summary="Websocket endpoint",
+   description="Connect to websocket",
+)
+@routes.get("/ws")
+async def websockets(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:  # type: WSMessage
+        if msg.type == WSMsgType.TEXT:
+            if msg.data == "/close":
+                await ws.close()
+            else:
+                data = loads(msg.data)
+                await send_message_to_all(data["message"])
+                await ws.send_str(msg.data)
+        elif msg.type == WSMsgType.ERROR:
+            logger.error(f"WS connection closed with exception {request.app.ws.exception()}")
+    return ws
+
+
 if __name__ == "__main__":
     app = web.Application()
     setup_aiohttp_apispec(
-        app=app, title="Andrew Bot documentation", version="v1.0",
+        app=app, title="Acronia Bot documentation", version="v1.0",
         url="/api/docs/swagger.json", swagger_path="/api/docs",
     )
     app.add_routes(routes)
